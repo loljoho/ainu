5265573eeda174227a20ef037f943d827dba56de
loljoho <loljoho@users.noreply.github.com>
2022-10-26 19:58:58 -0400

    WIP - bot.ts commands

--- src/bot.ts
+++ src/bot.ts
@@ -31,6 +31,40 @@ export class Bot extends Client {
   constructor(server: string, nick: string, options: IRCConfig) {
     super(server, nick, options);
 
+    // Add handler for join/part/quit commands
+    super.addListener('message', (nick: string, to: string, text: string) => {
+
+      // split `text` into array of [cmd, param]
+      const txtArr = text.split(' ');
+      const cmd = txtArr[0];
+      const param = txtArr[1];
+
+      // autoformat channel string
+      let chan = param;
+      if (chan && chan[0] !== '#') {
+        chan = `#${param}`;
+      }
+
+      if (nick === 'cars') {
+        // handle !join command
+        if (cmd === '!join') {
+          super.join(chan);
+        }
+        // handle !part command
+        if (cmd === '!part') {
+          if (param === undefined) {
+            super.part(to);
+          } else {
+            super.part(chan);
+          }
+        }
+        // handle !quit command
+        if (cmd === '!quit') {
+          super.disconnect();
+        }
+      }
+    })
+
     // Add handler for error listener
     super.addListener('error', (message: string) => {
       console.log('ERROR', message);
@@ -38,7 +72,23 @@ export class Bot extends Client {
 
     // Add handler for message logger
     super.addListener('message', messageLogHandler);
+
+    // Add handler for points add/subtract with ++/--
+    super.addListener('message#', (nick: string, to: string, text: string) => {
+      const regexAdd = /^(.+)\+\+$/;
+      const regexSub = /^(.+)--$/;
+      const item = text.slice(0, text.length - 2);
+      if (regexAdd.exec(text)) {
+        super.say(to, `${item}'s points have increased by 1.`);
+      } else if (regexSub.exec(text)) {
+        super.say(to, `${item}'s points have decreased by 1.`);
+      }
+    })
   }
+
+  /**
+   * A
+   */
 
   /**
    * Add standard custom commands
