9b6b2131af2e8c64b84f1e04d79bd7598fa79826
loljoho <loljoho@users.noreply.github.com>
2022-10-28 16:38:55 -0400

    WIP - weather module

--- src/utils/weather.ts
+++ src/utils/weather.ts
@@ -5,6 +5,8 @@
  *
  * @link https://openweathermap.org/current
  * @link https://openweathermap.org/api/geocoding-api
+ * @todo Implement non-US location parameters
+ * @todo Implement lat, lon lookup for location string
  */
 import axios from 'axios';
 
@@ -14,7 +16,7 @@ import { config } from '../config/config';
  * Current Weather Request
  *
  * @example
- * const temp = getCurrentWeather('08551').then(res => console.log(res));
+ * const temp = getCurrentWeather('10038').then(res => console.log(res));
  * @param {string} zipcode
  * @return {Promise<CurrentWeather>}
  */
@@ -27,10 +29,18 @@ export const getCurrentWeather = async (zipcode: string) => {
   return response.data;
 }
 
+/**
+ * Weather Forecast Request
+ * @example
+ * const temp = getForecast('10038').then(res => console.log(res));
+ * @param {string} zipcode
+ * @returns
+ */
 export const getForecast = async (zipcode: string) => {
   const apikey = config.env.OPENWEATHER_API_KEY;
-  // const units = 'imperial';
-  const path = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode}&appid=${apikey}`;
+  const units = 'imperial';
+  // const cnt = 0;  // number of timestamps to return
+  const path = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode}&appid=${apikey}&units=${units}`;
 
   const response = await axios.get(path);
 
