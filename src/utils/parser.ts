e468bc2af869130ea1125e8f6253ab01cbf52343
loljoho <loljoho@users.noreply.github.com>
2022-11-02 14:48:37 -0400

    WIP - utility to parse incoming messages

---
+++ src/utils/parser.ts
@@ -0,0 +1,30 @@
+// import { trigger } from '../config/config';
+
+/**
+ * Parse message commands
+ *
+ * Accepts message string as parameter; determines whether a valid command was triggered, and then
+ * returns the command and any additional parameters supplied.
+ *
+ * @param msg {string}
+ * @return
+ */
+// export const parseMessage = (msg: string) : { command: string; value: string; } => {
+//   const m = msg.split(' ');
+//   const cmd = m[0];
+
+//   // check for parameters supplied after the command
+//   if (m[1] !== undefined) {
+//     console.log('Found parameters');
+//   }
+
+//   //
+
+//   if (cmd[0] === trigger) {
+//     cmd.slice(1);
+//   }
+//   return {
+//     command: cmd;
+//     value: param;
+//   }
+// }
