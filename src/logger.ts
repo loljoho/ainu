0b8219d5bc2b6adf7fc76847c116b8fb231287eb
loljoho <loljoho@users.noreply.github.com>
2022-11-02 14:45:25 -0400

    Add janky temporary channel exclusion for logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,7 +2,13 @@ import dayjs, { type Dayjs } from 'dayjs';
 
 const DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
+/**
+ * @TODO Refactor and allow for disabling message logging
+ */
 export function messageLogHandler(nick: string, to: string, text: string, message: string) {
-  const timestamp: Dayjs = dayjs();
-  console.log(`${timestamp.format(DATETIME_FORMAT)}\t${to}\t<${nick}> ${text}`);
+  // Do not log the `NO_LOG_CHAN`
+  if (to !== `#${process.env.NO_LOG_CHAN}`) {
+    const timestamp: Dayjs = dayjs();
+    console.log(`${timestamp.format(DATETIME_FORMAT)} ${to}\t<${nick}> ${text}`);
+  }
 }
